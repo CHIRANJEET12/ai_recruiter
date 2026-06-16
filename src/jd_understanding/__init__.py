5e32e68c9358daa7611d9b0cc8e8055f757194a2
Akash-YS05 <akash.pan05@gmail.com>
2026-06-16 17:00:51 +0530

    JD understanding modules

---
+++ src/jd_understanding/__init__.py
@@ -0,0 +1,10 @@
+from jd_understanding.parser import JDParserEngine
+from jd_understanding.policy import WeightPolicyEngine
+from jd_understanding.schemas import ParsedJD, ScoreWeights
+
+__all__ = [
+    "JDParserEngine",
+    "WeightPolicyEngine",
+    "ParsedJD",
+    "ScoreWeights",
+]
